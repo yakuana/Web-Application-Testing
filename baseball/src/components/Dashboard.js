70b527f144855479764aa91d4fa4e828c6eecb61
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-08 14:55:02 -0500

    WIP: Created Dashboard.js class.

---
+++ baseball/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+
+    state = {
+        
+    }
+
+    render() {
+        return (
+            <div className="dashboard-container">
+                <button>strike</button>
+                <button>ball</button>
+                <button>foul</button>
+                <button>hit</button>
+            </div>
+        )
+    }  
+}
+
+export default Dashboard; 
